d2f488d18ed31c05295e2c34fd645c5959652d5a
RainMonster <the.rain.monster@gmail.com>
2019-09-05 22:40:20 -0400

    moblile menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+function classToggle() {
+  const navs = document.querySelectorAll('.menu')
+  navs.forEach(nav => nav.classList.toggle('menu-toggleShow'));
+}
+
 const fileUrl = '../banner.html';
 
 fetch(fileUrl)
@@ -9,6 +14,10 @@ fetch(fileUrl)
     var doc = parser.parseFromString(html, "text/html");
     var headerContainer = document.querySelector('.header-container');
     headerContainer.appendChild(doc.querySelector('.header'));
+    var hamburglar = document.querySelector('.menu-toggle');
+    console.log("loaded: " + hamburglar);
+    document.querySelector('.menu-toggle')
+        .addEventListener('click', classToggle)
   })
   .catch(function(err) {
     console.log('Failed to fetch page: ', err);
